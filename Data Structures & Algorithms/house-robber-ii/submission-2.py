f790791517fb67dd7536495451526cbc2986161d
Prasanna-cpu <76141090+Prasanna-cpu@users.noreply.github.com>
2026-05-07 08:15:06 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def rob_linear(arr):
+            prev2 = 0
+            prev1 = 0
+
+            for num in arr:
+                curr = max(prev1, prev2 + num)
+                prev2 = prev1
+                prev1 = curr
+            
+            return prev1
+
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+
+        case1 = rob_linear(nums[:-1])
+        case2 = rob_linear(nums[1:])
+
+        return max(case1, case2)
